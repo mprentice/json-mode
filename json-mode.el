8133aba613e374082a20fe748363b68a778a162c
Mike Prentice <michael.prentice@tufts.edu>
2012-12-26 15:37:06 -0500

    Use json-lint-command custom variable
    Default to jsonlint --format

--- json-mode.el
+++ json-mode.el
@@ -8,12 +8,12 @@
 (defvar json-mode-hook nil)
 
 (defconst json-quoted-key-re "\\(\"[^\"]+?\"[ ]*:\\)")
-(defconst json-quoted-string-re "\\(\".*?\"\\)") 
+(defconst json-quoted-string-re "\\(\".*?\"\\)")
 (defconst json-number-re "[^\"]\\([0-9]+\\(\\.[0-9]+\\)?\\)[^\"]")
 (defconst json-keyword-re "\\(true\\|false\\|null\\)")
 
 (defconst json-font-lock-keywords-1
-  (list 
+  (list
    (list json-quoted-key-re 1 font-lock-keyword-face)
    (list json-quoted-string-re 1 font-lock-string-face)
    (list json-keyword-re 1 font-lock-constant-face)
@@ -21,12 +21,17 @@
    )
   "Level one font lock.")
 
-(defun beautify-json ()
+(defcustom json-lint-command
+  "jsonlint --format"
+  "Command to pretty-print JSON"
+  :type '(string))
+
+(defun json-lint ()
   (interactive)
   (let ((b (if mark-active (min (point) (mark)) (point-min)))
         (e (if mark-active (max (point) (mark)) (point-max))))
     (shell-command-on-region b e
-     "python -mjson.tool" (current-buffer) t)))
+     json-lint-command (current-buffer) t)))
 
 (define-derived-mode json-mode javascript-mode "JSON"
   "Major mode for editing JSON files"
